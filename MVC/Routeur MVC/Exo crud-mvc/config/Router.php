02dc40bffa3932c1d962b946116ec815cb31a8a0
Khardiata.Diop <khardiata.diop@outlook.com>
2025-01-23 11:02:50 +0000

    etape 1

--- MVC/Routeur MVC/Exo crud-mvc/config/Router.php
+++ MVC/Routeur MVC/Exo crud-mvc/config/Router.php
@@ -8,20 +8,33 @@
     
         public function handleRequest(array $get) : void
         {
-            if(isset($get["route"]) && $get["route"] === "a-propos"){
-                $prems = new UserController();
-                $prems->about();
+            if(isset($get["route"]) && $get["route"] === "show_user"){
+                $leshow = new UserController();
+                $leshow->show();
                 
                 
-            } else if(! isset($get["route"])){
-                $sec = new UserController();
-                $sec->home();
+            } else if($get["route"] === "create_user"){
+                $lecreate = new UserController();
+                $lecreate->create();
                 
                 
+            }else if($get["route"] === "check_create_user"){
+                $leCheckCreate = new UserController();
+                $leCheckCreate->checkCreate();
+            }else if($get["route"] === "update_user"){
+                $leUpdateUser = new UserController();
+                $leUpdateUser->update();
+            }else if( $get["route"] === "check_update_user"){
+                $leCkeckUpdate = new UserController();
+                $leCkeckUpdate->checkUpdate();
+            }else if($get["route"] === "delete_user"){
+                $leDelete = new UserController();
+                $leDelete->delete();
             }else{
                 $autre = new UserController();
-                $autre->notFound();
+                $autre->list();
             }
+            
         }
     
     }
